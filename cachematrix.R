c12ba44e20efe899ede9477b1b6c1a19a43724d3
gkrangi <102710713+gkrangi@users.noreply.github.com>
2022-04-02 11:32:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 ## The following function creates a special "matrix" object to cache its inverse.
 ## It is assumed that the matrix supplied is always invertible (square matrix).
 
-makeCacheMatrix <- function(x = matrix()) { # define the argument with default mode 'matrix'
+makeCacheMatrix<-function(x=matrix()){      # define the argument with default mode "matrix"
   inv<-NULL                                 # initialize inv as NULL to store the value of inverse matrix
-  set<-function(y){                         #  define the set function to assign new value of matrix in parent environment
+  set<-function(y){                         # define the set function to assign new value of matrix in parent environment
   x<<-y
   inv<<-NULL                                #  reset inv to NULL if there's a new matrix
   }
   get<-function()x                          # define the get function to return value of the matrix argument
   
-  setinverse<-function(inverse) inv<<-inverse #assigning value of inv in parent environment
+  setinverse<-function(inverse) inv<<-inverse # assigning value of inv in parent environment
   getinverse<-function() inv                  # gets the value of inv when called 
   list(set = set, get = get,                  # assigns each function names to access them using $
        setinverse = setinverse,
