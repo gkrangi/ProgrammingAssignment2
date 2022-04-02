29739c9f8cbf0d16c175aa5e1024f7e367a9808b
gkrangi <102710713+gkrangi@users.noreply.github.com>
2022-04-02 10:42:26 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The following function creates a special "matrix" object to cache its inverse.
+## It is assumed that the matrix supplied is always invertible (square matrix).
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { # define the argument with default mode 'matrix'
+  inv<-NULL                                 # initialize inv as NULL to store the value of inverse matrix
+  set<-function(y){                         #  define the set function to assign new value of matrix in parent environment
+  x<<-y
+  inv<<-NULL                                #  reset inv to NULL if there's a new matrix
+  }
+  get<-function()x                          # define the get function to return value of the matrix argument
+  
+  setinverse<-function(inverse) inv<<-inverse #assigning value of inv in parent environment
+  getinverse<-function() inv                  # gets the value of inv when called 
+  list(set = set, get = get,                  # assigns each function names to access them using $
+       setinverse = setinverse,
+       getinverse = getinverse)  
 
 }
 
-
 ## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated then cacheSolve will get the inverse from cache given the matrix remains unchanged.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinverse()
+  if(!is.null(inv)) {                       # checking if inverse is NULL
+    message("getting cached data")
+    return(inv)                             # returns inverse value
+  }
+  data <- x$get()
+  inv <- solve(data, ...)                   # calculates inverse value
+  x$setinverse(inv)
+  inv                                       # returns matrix inverse of 'x'
 }
+
